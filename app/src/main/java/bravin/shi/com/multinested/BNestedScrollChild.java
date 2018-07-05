a5080ec34fb7d09e4f508cd30523364ea930e9ba
bravin <470726148@qq.com>
2018-07-05 18:09:30 +0800

    create

---
+++ app/src/main/java/bravin/shi/com/multinested/BNestedScrollChild.java
@@ -0,0 +1,12 @@
+package bravin.shi.com.multinested;
+
+import android.support.v4.view.NestedScrollingChild2;
+
+/**
+ * created by bravin on 2018/7/5.
+ */
+public interface BNestedScrollChild extends NestedScrollingChild2 {
+    boolean canScroll2Top();
+
+    boolean canScroll2Bottom();
+}
