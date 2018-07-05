3f7f95863556bc526e3ac4bd3fda8c1343546dbb
bravin <470726148@qq.com>
2018-07-05 18:34:12 +0800

    coding

--- app/src/main/java/bravin/shi/com/multinested/MultiNestedParentLayout.java
+++ app/src/main/java/bravin/shi/com/multinested/MultiNestedParentLayout.java
@@ -3,9 +3,13 @@ package bravin.shi.com.multinested;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.view.NestedScrollingParent;
 import android.support.v4.view.NestedScrollingParent2;
+import android.support.v4.widget.NestedScrollView;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 /**
@@ -23,11 +27,18 @@ public class MultiNestedParentLayout extends LinearLayout implements NestedScrol
 
     public MultiNestedParentLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        setOrientation(VERTICAL);
     }
 
-    private void init() {
-        setOrientation(LinearLayout.VERTICAL);
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setOrientation(VERTICAL);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
     }
 
     @Override
