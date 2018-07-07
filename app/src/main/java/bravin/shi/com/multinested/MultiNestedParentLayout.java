10be04b8c74ec1f83a685abc9e0be4d0ade42b4a
bravinshi <470726148@qq.com>
2018-07-07 21:57:27 +0800

    MultiNestedParentLayout

--- app/src/main/java/bravin/shi/com/multinested/MultiNestedParentLayout.java
+++ app/src/main/java/bravin/shi/com/multinested/MultiNestedParentLayout.java
@@ -3,13 +3,22 @@ package bravin.shi.com.multinested;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.NestedScrollingChild2;
 import android.support.v4.view.NestedScrollingParent2;
+import android.support.v4.view.NestedScrollingParentHelper;
+import android.support.v4.view.ScrollingView;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewParent;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,12 +29,18 @@ import bravin.shi.com.multinested.utils.ScreenUtils;
  * created by bravin on 2018/7/5.
  */
 public class MultiNestedParentLayout extends LinearLayout implements
-        NestedScrollingParent2 {
+        NestedScrollingParent2, ScrollingView {
 
     private static final String TAG = "Multi";
+    private static final String TAG1 = "Scroll";
     private int screenHeight;
 
+    private int virtualOffsetY = 0;
+
+    private NestedScrollingParentHelper scrollingParentHelper;
+
     private List<NestedScrollingChild2> nestedChildren = new ArrayList<>();
+    private List<ScrollingView> scrollingChildren = new ArrayList<>();
 
     public MultiNestedParentLayout(Context context) {
         this(context, null);
@@ -35,10 +50,10 @@ public class MultiNestedParentLayout extends LinearLayout implements
         this(context, attrs, 0);
     }
 
-    public MultiNestedParentLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public MultiNestedParentLayout(Context context, @Nullable AttributeSet attrs,
+                                   int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         screenHeight = ScreenUtils.getScreenHeight();
-        Log.d(TAG, "screenHeight: " + screenHeight);
         setOrientation(VERTICAL);
     }
 
@@ -48,6 +63,7 @@ public class MultiNestedParentLayout extends LinearLayout implements
         final int count = getChildCount();
         Log.d(TAG, "children count: " + count);
         nestedChildren.clear();
+        scrollingChildren.clear();
         final int parentHeightMode = MeasureSpec.getMode(heightMeasureSpec);
         final int parentHeight = MeasureSpec.getSize(heightMeasureSpec);
 
@@ -56,57 +72,65 @@ public class MultiNestedParentLayout extends LinearLayout implements
                 || parentHeightMode == MeasureSpec.UNSPECIFIED) {
             // 循环测量父布局避免可滚动的child大小超过屏幕大小
             boolean hasParent = true;
-            while (hasParent){
-                if (tempHeight > 0 && tempHeight <= screenHeight){
+            while (hasParent) {
+                if (tempHeight > 0 && tempHeight <= screenHeight) {
                     hasParent = false;
-                }else {
+                } else {
                     ViewParent viewParent = getParent();
-                    if (viewParent != null){
+                    if (viewParent != null) {
                         View parent = (View) viewParent;
                         tempHeight = parent.getMeasuredHeight();
-                    }else {
+                    } else {
                         tempHeight = screenHeight;
                         hasParent = false;
                     }
                 }
             }
-        }else {
-            if (tempHeight > screenHeight){
+        } else {
+            if (tempHeight > screenHeight) {
                 tempHeight = screenHeight;
             }
         }
         int childHeightMeasureSpec =
                 MeasureSpec.makeMeasureSpec(tempHeight, MeasureSpec.AT_MOST);
 
-        for (int i = 0; i < count;i++){
+        for (int i = 0; i < count; i++) {
             View child = getChildAt(i);
             if (child == null) {
                 Log.w(TAG, "child is null at " + i);
                 continue;
             }
-            if (child.getVisibility() == GONE){
+            if (child.getVisibility() == GONE) {
                 continue;
             }
+            final LinearLayout.LayoutParams lp =
+                    (LinearLayout.LayoutParams) child.getLayoutParams();
+            final LayoutParams newLayoutParams = new LayoutParams(lp);
+
             if (child instanceof NestedScrollingChild2) {
                 nestedChildren.add((NestedScrollingChild2) child);
             }
-            final LayoutParams lp = (LayoutParams) child.getLayoutParams();
+            if (child instanceof ScrollingView) {
+                scrollingChildren.add((ScrollingView) child);
+                newLayoutParams.setScrollingView(true);
+            }
+            child.setLayoutParams(newLayoutParams);
             int tempMS;
-            if (lp.height > 0){
-                tempMS = MeasureSpec.makeMeasureSpec(lp.height, MeasureSpec.EXACTLY);
+            if (newLayoutParams.height > 0) {
+                tempMS = MeasureSpec.makeMeasureSpec(newLayoutParams.height, MeasureSpec.EXACTLY);
                 child.measure(widthMeasureSpec, tempMS);
-            }else {
+            } else {
                 child.measure(widthMeasureSpec, childHeightMeasureSpec);
             }
             final int childHeight = child.getMeasuredHeight();
             Log.d(TAG, "childHeight: " + childHeight);
 
-            mTotalLength += childHeight + lp.topMargin + lp.bottomMargin;
+            mTotalLength += childHeight + newLayoutParams.topMargin + newLayoutParams.bottomMargin;
         }
         Log.d(TAG, "mTotalLength1: " + mTotalLength);
 
         if (parentHeightMode == MeasureSpec.EXACTLY
-                || parentHeightMode == MeasureSpec.AT_MOST){
+                || parentHeightMode == MeasureSpec.AT_MOST) {
             if (mTotalLength > parentHeight) {
                 mTotalLength = parentHeight;
             }
@@ -118,27 +142,311 @@ public class MultiNestedParentLayout extends LinearLayout implements
     }
 
     @Override
-    public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes, int type) {
-        return false;
+    public boolean onStartNestedScroll(@NonNull View child, @NonNull View target,
+                                       int axes, int type) {
+        Log.d(TAG1, "onStartNestedScroll");
+        return type == ViewCompat.TYPE_TOUCH
+                && (axes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0 && canScroll();
     }
 
     @Override
-    public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, int axes, int type) {
-
+    public void onNestedScrollAccepted(@NonNull View child, @NonNull View target,
+                                       int axes, int type) {
+        Log.d(TAG1, "onNestedScrollAccepted");
     }
 
     @Override
     public void onStopNestedScroll(@NonNull View target, int type) {
-
+        Log.d(TAG1, "onStopNestedScroll");
     }
 
     @Override
-    public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
-
+    public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed,
+                               int dxUnconsumed, int dyUnconsumed, int type) {
+        Log.d(TAG1, "onNestedScroll");
     }
 
     @Override
-    public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
+    public void onNestedPreScroll(@NonNull View target, int dx, int dy,
+                                  @NonNull int[] consumed, int type) {
+        Log.d(TAG1, "onNestedPreScroll");
+        // 可以改成支持滑动 这样就成了可以滑动的LinearLayout
+        if (scrollingChildren.size() == 0 || dy == 0) {
+            return;
+        }
+        final int scrollRange = getScrollRange();
+        // Y轴最大可偏移距离
+        int maxOffsetY = scrollRange - getHeight() - getPaddingTop() - getPaddingBottom();
+        // 如果不能滑动，return
+        if (dy > 0 && (maxOffsetY <= 0 || maxOffsetY == virtualOffsetY)) {
+            return;
+        }
+        if (dy < 0 && virtualOffsetY == 0) {
+            return;
+        }
 
+        if (dy > 0) {
+            // 计算此次滚动的可处理距离
+            int canConsumed = Math.min(maxOffsetY - virtualOffsetY, dy);
+            consumed[1] = canConsumed;
+            handleScrolling2Down(canConsumed, 0);
+        } else {
+
+        }
+    }
+
+    /**
+     * 处理向下滚动
+     *
+     * @param consumed
+     * @param skipNum
+     */
+    private void handleScrolling2Down(int consumed, int skipNum) {
+
+        final int count = getChildCount();
+        if (skipNum >= count || consumed <= 0) {
+            return;
+        }
+        for (int i = skipNum; i < count; i++) {
+            final View view = getChildAt(i);
+            if (view.getVisibility() == GONE) {
+                // If the child is GONE, skip...
+                continue;
+            }
+            final LayoutParams layoutParams = (LayoutParams) view.getLayoutParams();
+            int bottomDifference = viewToBottomDistance(view);
+            if (!layoutParams.isScrollingView()) {// 不是ScrollingView
+                if (bottomDifference >= consumed) {
+                    virtualScrollBy(consumed);
+                } else if (bottomDifference > 0) {
+                    virtualScrollBy(bottomDifference);
+                    handleScrolling2Down(consumed - bottomDifference, i + 1);
+                } else {
+                    // 如果这个child显示已经在bottom最低点之上，直接跳过
+                    handleScrolling2Down(consumed, i + 1);
+                }
+            } else {// ScrollingView
+                if (bottomDifference >= consumed){
+                    virtualScrollBy(consumed);
+                }else if (bottomDifference >= 0) {
+                    virtualScrollBy(bottomDifference);// 先移动view 再考虑view内部
+                    int rest = consumed - bottomDifference;
+                    if (view.canScrollVertically(1)){
+                        int canOffset = getViewOffsetDistanceToMax((ScrollingView) view);
+                        if (canOffset >= rest){
+                            virtualScrollBy(rest, (ScrollingView) view);
+                        }else {
+                            virtualScrollBy(canOffset, (ScrollingView) view);
+                            handleScrolling2Down(rest - canOffset, i + 1);
+                        }
+                    }else {// 不可滚动时 直接跳过
+                        handleScrolling2Down(consumed - bottomDifference, i + 1);
+                    }
+                }else {
+                    // 如果这个child显示已经在bottom最低点之上，直接跳过
+                    handleScrolling2Down(consumed, i + 1);
+                }
+            }
+        }
+    }
+
+    private int getViewOffsetDistanceToMax(ScrollingView view){
+        int distance = view.computeVerticalScrollRange() - view.computeVerticalScrollOffset();
+        View v = (View) view;
+        distance -= v.getPaddingTop() - v.getPaddingBottom() - v.getHeight();
+        Log.d(TAG1, "distance: " + distance);
+        return distance;
+    }
+
+    private void virtualScrollBy(int dy) {
+        Log.d("aaa",getChildAt(0).getBottom() + "");
+        scrollBy(0, dy);
+        virtualOffsetY += dy;
+        post(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("aaa",getChildAt(0).getBottom() + "");
+            }
+        });
+    }
+
+    private void virtualScrollBy(int dy, ScrollingView scrollingView) {
+        Log.d("aaa",getChildAt(0).getBottom() + " sv");
+        View view = (View) scrollingView;
+        view.scrollBy(0,dy);
+        virtualOffsetY += dy;
+        post(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("aaa",getChildAt(0).getBottom() + " sv");
+            }
+        });
+    }
+
+    private int viewToBottomDistance(View view) {
+        final int bottom = getHeight() - getPaddingBottom();
+        Log.d(TAG, "bottom: " + bottom);
+        Log.d(TAG, "view.getBottom(): " + view.getBottom());
+        Log.d(TAG,  "viewToBottomDistance: " + (view.getBottom() - bottom));
+        return view.getBottom() - bottom - getScrollY();
+    }
+
+    /**
+     * 判断是否有ScrollingView显示不完全
+     *
+     * @return
+     */
+    private boolean hasScrollViewShowingHalf() {
+        return true;
+    }
+
+    /**
+     * 获取最靠近底部的可以看见的Scrolling View
+     *
+     * @return
+     */
+    private View getMostAlignBottomShowingScrollingView() {
+        int scrollingViewCount = scrollingChildren.size();
+        int top = getHeight() - getPaddingTop();
+        int bottom = getHeight() - getPaddingBottom();
+        for (int i = 0; i < scrollingViewCount; i++) {
+            View view = (View) scrollingChildren.get(i);
+            LayoutParams lp = (LayoutParams) view.getLayoutParams();
+//            if (view.getTop() > bottom && view.getBottom() < top)
+        }
+        return null;
+    }
+
+    private int getLastViewBottomWithMargin() {
+        final int count = getChildCount();
+        for (int i = count - 1; i > -1; i--) {
+            final View view = getChildAt(i);
+            if (view.getVisibility() == GONE) {
+                // If the child is GONE, skip...
+                continue;
+            }
+            LayoutParams layoutParams = (LayoutParams) view.getLayoutParams();
+            return view.getBottom() + layoutParams.bottomMargin;
+        }
+        return -1;
+    }
+
+    private NestedScrollingParentHelper getScrollingParentHelper() {
+        if (scrollingParentHelper == null) {
+            scrollingParentHelper = new NestedScrollingParentHelper(this);
+        }
+        return scrollingParentHelper;
+    }
+
+    private int scrollRange;
+
+    private int getScrollRange(){
+        return scrollRange;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        computeVerticalScrollRange();
+    }
+
+    @Override
+    public int computeVerticalScrollRange() {
+
+        final int count = getChildCount();
+        final int contentHeight = getHeight() - getPaddingBottom() - getPaddingTop();
+        if (count == 0) {
+            return contentHeight;
+        }
+        int range = 0;
+        ViewGroup.MarginLayoutParams layoutParams;
+        View child;
+        for (int i = 0; i < count; i++) {
+            child = getChildAt(i);
+            if (child == null || child.getVisibility() == GONE) {
+                continue;
+            }
+            layoutParams = (MarginLayoutParams) child.getLayoutParams();
+            if (child instanceof ScrollingView) {
+                range += ((ScrollingView) child).computeVerticalScrollRange()
+                        + child.getPaddingTop() + child.getPaddingBottom()
+                        + layoutParams.topMargin + layoutParams.bottomMargin;
+            } else {
+                range += child.getHeight() + layoutParams.topMargin
+                        + layoutParams.bottomMargin;
+            }
+        }
+        Log.d(TAG1, "computeVerticalScrollRange: " + range);
+        scrollRange = range;
+        return range;
+    }
+
+    /*
+     * 判断layout当前能否在竖直方向滑动
+     */
+    private boolean canScroll() {
+        return getScrollRange() > getHeight()
+                - getPaddingTop() - getPaddingBottom();
+    }
+
+    @Override
+    public int computeVerticalScrollOffset() {
+        return virtualOffsetY;
+    }
+
+    @Override
+    public int computeVerticalScrollExtent() {
+        return 0;
+    }
+
+    @Override
+    public int computeHorizontalScrollRange() {
+        return 0;
+    }
+
+    @Override
+    public int computeHorizontalScrollOffset() {
+        return 0;
+    }
+
+    @Override
+    public int computeHorizontalScrollExtent() {
+        return 0;
+    }
+
+    public static class LayoutParams extends LinearLayout.LayoutParams {
+        private boolean isScrollingView = false;
+
+        public LayoutParams(Context c, AttributeSet attrs) {
+            super(c, attrs);
+        }
+
+        public LayoutParams(int width, int height) {
+            super(width, height);
+        }
+
+        public LayoutParams(int width, int height, float weight) {
+            super(width, height, weight);
+        }
+
+        public LayoutParams(ViewGroup.LayoutParams p) {
+            super(p);
+        }
+
+        public LayoutParams(MarginLayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(LinearLayout.LayoutParams source) {
+            super(source);
+        }
+
+        public boolean isScrollingView() {
+            return isScrollingView;
+        }
+
+        public void setScrollingView(boolean scrollingView) {
+            isScrollingView = scrollingView;
+        }
     }
 }
---
+++ app/src/main/java/bravin/shi/com/multinested/NestedScrollingView.java
@@ -0,0 +1,7 @@
+package bravin.shi.com.multinested;
+
+import android.support.v4.view.NestedScrollingChild2;
+import android.support.v4.view.ScrollingView;
+
+public interface NestedScrollingView extends ScrollingView, NestedScrollingChild2 {
+}
